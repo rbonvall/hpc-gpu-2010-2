bfe4d988ae5dc96d3bb36d49cab8347219eef672
Roberto Bonvallet <rbonvall@gmail.com>
2010-09-10 20:30:41 -0400

    gpumap

--- tarea-1/gpu-map.cu
+++ tarea-1/gpu-map.cu
@@ -30,38 +30,7 @@ __global__ void map_f(float x[]) {
 
 void gpu_map(float x[], unsigned n) {
 
-    unsigned mem_size = sizeof(float) * n;
-     
-    float *d_x;
-    cudaMalloc((void **) &d_x, n * sizeof(float));
-    cudaMemcpy(d_x, x, mem_size, cudaMemcpyHostToDevice);
+    // Juegue.
 
-    dim3 grid_size, block_size;
-    block_size.x = 512;      // number of threads per block (<= 512)
-    grid_size.x = n / 512;   // number of blocks
-
-    cudaEvent_t start, stop;
-    cudaEventCreate(&start);
-    cudaEventCreate(&stop);
-
-    cudaEventRecord(start, 0);
-    map_f<<<grid_size, block_size>>>(d_x);
-    cudaEventRecord(stop, 0);
-    cudaEventSynchronize(stop);
-
-    float time;
-    cudaEventElapsedTime(&time, start, stop);
-    cudaEventDestroy(start);
-    cudaEventDestroy(stop);
-
-    cudaError_t error = cudaGetLastError();
-    if (error != cudaSuccess) {
-        printf("CUDA error: %s\n", cudaGetErrorString(error));
-        exit(-1);
-    }
-
-    cudaMemcpy(x, d_x, mem_size, cudaMemcpyDeviceToHost);
-    cudaFree(d_x);
-    d_x = 0;
 }
 
