1e3454bd9ae61860c33f97c819570cd2c8ca57ec
Roberto Bonvallet <rbonvall@gmail.com>
2010-10-07 13:54:08 -0400

    Nueva función a integrar en plot.py

--- tarea-2/plot.py
+++ tarea-2/plot.py
@@ -1,17 +1,24 @@
 #!/usr/bin/env python
+# encoding: utf-8
 
 from __future__ import division
 from numpy import *
 import scipy.integrate
 import pylab
 
+def k(x):
+    return .5 + 1.5 / (1 + 50 * x**2)
+
 def f(x):
-    return 2 + 9 / (1 + 600 * (x - .43)**2)
+    xs = [-3.3 + i * 0.7 for i in range(10)]
+    return sum(k(x - x0) for x0 in xs) / len(xs)
 
 def g(x):
-    return exp(-x/4) * cos(2 * pi * f(x) * x)**2
+    return exp(-x/16) * cos(2 * pi * f(x) * x)**2
 
-x = linspace(0, 1, 2**20 + 1).astype(float32)
+a, b = -4.0, 4.0
+n = 2**22          # número de intervalos
+x = linspace(a, b, n + 1).astype(float32)
 
 # compute integral using Simpson's rule
 integral = scipy.integrate.simps(g(x), x)
