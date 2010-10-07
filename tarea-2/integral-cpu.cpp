7cd0f772b8e0c0b6f3f9084804f6309e408d51dc
Roberto Bonvallet <rbonvall@gmail.com>
2010-10-07 13:53:47 -0400

    Nueva función a integrar en integral-cpu.cpp

--- tarea-2/integral-cpu.cpp
+++ tarea-2/integral-cpu.cpp
@@ -4,19 +4,27 @@
 
 #define SQ(x) ((x) * (x))
 
-static const float A = 0.0, B = 1.0;  // limites de integracion
-static const int N = 1 << 20;         // numero de intervalos = 2^20
+static const float A = -4.0, B = 4.0;  // limites de integracion
+static const int N = 1 << 22;         // numero de intervalos = 2^20
 static const float H = (B - A) / N;   // taman~o del intervalo de integracion
 static const float PI(M_PI);          // pi con precision simple
 
+float h(float x) {
+    return .5f + 1.5f / (1.0f + 50.0f * SQ(x));
+}
+
 float f(float x) {
-    float d = x - .43f;
-    return 2.0f + 9.0f / (1.0f + 600.0f * SQ(d));
+    int i;
+    float sum = 0.0f, x0;
+    for (i = 0; i < 10; ++i)
+        x0 = -3.3f + i * 0.7f;
+        sum += h(x - x0);
+    return sum/10.0f;
 }
 
 float g(float x) {
     float c = std::cos(2.0f * PI * f(x) * x);
-    return std::exp(-x/4.0f) * SQ(c);
+    return std::exp(-x/16.0f) * SQ(c);
 }
 
 int main() {
