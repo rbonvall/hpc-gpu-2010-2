0b5d0ab22445def548a4989ca95ff1c1621e9526
Roberto Bonvallet <rbonvall@gmail.com>
2010-10-07 22:15:44 -0400

    Comentarios a código de referencia

--- tarea-2/integral-cpu.cpp
+++ tarea-2/integral-cpu.cpp
@@ -4,10 +4,10 @@
 
 #define SQ(x) ((x) * (x))
 
-static const float A = -4.0, B = 4.0;  // limites de integracion
-static const int N = 1 << 22;         // numero de intervalos = 2^20
-static const float H = (B - A) / N;   // taman~o del intervalo de integracion
-static const float PI(M_PI);          // pi con precision simple
+static const float A = -4.0, B = 4.0;  // limites de integración
+static const int N = 1 << 22;          // número de intervalos = 2^20
+static const float H = (B - A) / N;    // tamaño del intervalo de integración
+static const float PI(M_PI);           // π con precision simple
 
 float h(float x) {
     return .5f + 1.5f / (1.0f + 50.0f * SQ(x));
@@ -31,6 +31,15 @@ int main() {
     float sum = 0.0;
     float x;
 
+    // Regla de Simpson:
+    // se evalúa la función g tanto en los puntos frontera entre los subintervalos como
+    // en los puntos medios.
+    // * los extremos A y B del dominio se ponderan con factor 1;
+    // * los puntos medios de cada subintervalo se ponderan con factor 4;
+    // * los puntos frontera entre subintervalos se ponderan con factor 2.
+    // El valor aproximado de la integral es
+    // H/6 por la suma ponderada de las evaluaciones.
+
 #   pragma omp parallel for private(x) reduction(+: sum)
     for (int i = 1; i < 2 * N; ++i) {
         x = A + i * (H / 2);
@@ -41,5 +50,15 @@ int main() {
     float integral = sum * H / 6.0f;
 
     std::cout << std::setprecision(5) << integral << std::endl;
+
+    // La integración está paralelizada usando OpenMP.
+    // Haga la prueba de correr el programa paralelo (con el pragma)
+    // y serial (sin el pragma) en un computador multicore.
+    //
+    // ¿Cambia el tiempo de ejecución?
+    // ¿Es lineal la mejora (8 cores ⇒ tiempo baja 1/8)?
+    // Puede medir el tiempo ejecutando así:  time ./integral-cpu
+    //
+    // ¿Cambia el resultado de la integral? ¿Por qué?
 }
 
